f11901a3d403a81cb4c21e3d63828b0ef12c59b6
Himanshu <69390482+himanshukharwar@users.noreply.github.com>
2023-10-10 17:36:39 +0530

    installed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-// import './App.css'
+// import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      {/* <h1>Chain-Aur-React With VITE🔥</h1> */}
+      <div className="relative h-[400px] w-[300px] rounded-md">
+        <img
+          src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="AirMax Pro"
+          className="z-0 h-full w-full rounded-md object-cover"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+        <div className="absolute bottom-4 left-4 text-left">
+          <h1 className="text-lg font-semibold text-white">Delba</h1>
+          <p className="mt-2 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+            debitis?
+          </p>
+          <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+            View Profile →
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
